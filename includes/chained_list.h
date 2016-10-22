1eb7a3b539fec4bd2cf16003fc0d7fac5ad26b16
rmasse <roman.masse@gmail.com>
2016-10-22 11:41:09 +0200

    Add basic chained list

---
+++ includes/chained_list.h
@@ -0,0 +1,16 @@
+#ifndef CHAINED_LIST_H
+# define CHAINED_LIST_H
+
+#include <stdlib.h>
+
+typedef struct		s_list
+{
+	void			*data;
+	struct s_list	*next;
+}					t_list;
+
+void		list_append(t_list *lst, void *data);
+void		list_map(t_list *lst, void (*f)(t_list, void *), void *args);
+t_list		*list_new_node(void *data);
+
+#endif
---
+++ srcs/chained_list.c
@@ -0,0 +1,33 @@
+#include "chained_list.h"
+
+void		list_append(t_list *lst, void *data)
+{
+	t_list	*it;
+
+	it = lst;
+	while (it->next)
+		it = it->next;
+	it->next = list_new_node(data);
+}
+
+void		list_map(t_list *lst, void (*f)(t_list, void *), void *args)
+{
+	t_list	*it = lst;
+
+	while (it)
+	{
+		f(*it, args);
+		it = it->next;
+	}
+}
+
+t_list		*list_new_node(void *data)
+{
+	t_list	*new_node;
+
+	if (!(new_node = (t_list *)malloc(sizeof(t_list))))
+		return NULL;
+	new_node->data = data;
+	new_node->next = NULL;
+	return (new_node);
+}
